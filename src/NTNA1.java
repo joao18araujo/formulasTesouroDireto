bfd2f59e2c3477556df24627a429c54640d06673
Mateus Manuel <mateusmanuel@unb.br>
2017-05-19 13:43:59 -0300

    Creating NTNA1 class just to test pass
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>
    Signed-off-by: Mateus Manuel <mateusmanuel21@gmail.com>

---
+++ src/NTNA1.java
@@ -0,0 +1,43 @@
+
+public class NTNA1 {
+	
+	float baseDolarQuotation;
+	float endDolarQuotation;
+	float liquidValue;
+	
+	public NTNA1(float baseDolarQuotation, float endDolarQuotation, float liquidValue) {
+		super();
+		this.baseDolarQuotation = baseDolarQuotation;
+		this.endDolarQuotation = endDolarQuotation;
+		this.liquidValue = liquidValue;
+	}
+	
+	public NTNA1() {
+	}
+
+	public float getBaseDolarQuotation() {
+		return baseDolarQuotation;
+	}
+	public void setBaseDolarQuotation(float baseDolarQuotation) {
+		this.baseDolarQuotation = baseDolarQuotation;
+	}
+	public float getEndDolarQuotation() {
+		return endDolarQuotation;
+	}
+	public void setEndDolarQuotation(float endDolarQuotation) {
+		this.endDolarQuotation = endDolarQuotation;
+	}
+	public float getLiquidValue() {
+		return liquidValue;
+	}
+	public void setLiquidValue(float liquidValue) {
+		this.liquidValue = liquidValue;
+	}
+
+	public float calculatePUAmortization() {
+		return 0.0F;
+	}
+
+	
+
+}
