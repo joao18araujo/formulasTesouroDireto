4119892436a8ade60fa43c8fd911c9733bb628a0
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-19 15:21:12 -0300

    Creating blank method to UpdatedNominalValue

---
+++ src/NTNA6.java
@@ -0,0 +1,12 @@
+
+public class NTNA6 {
+	private double VE;
+	
+	public void setVE(double ve){
+		VE = ve;
+	}
+
+	public double calculateUpdatedNominalValue(double ussa, double ussb){	
+		return 0;
+	}
+}
