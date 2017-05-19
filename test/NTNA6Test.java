5e4f76eb99c657cd1a4ff2e4c5f05a0b16a0949a
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-19 16:15:52 -0300

    Creating interestCalculation test

--- test/NTNA6Test.java
+++ test/NTNA6Test.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.*;
 
 public class NTNA6Test {
@@ -18,4 +23,15 @@ public class NTNA6Test {
 		assertEquals(1436.454268, ntna6.calculateUpdatedNominalValue(ussa, ussb), 1e-6);
 	}
 	
+	@Test
+	public void testInterestCalculation() throws ParseException{
+		Date dtp = new SimpleDateFormat("dd/MM/yyyy").parse("10/05/2017");
+		Date dtup = new SimpleDateFormat("dd/MM/yyyy").parse("30/06/2017");
+		double interest = 4;
+		
+		ntna6.setVNA(2350.56);
+		
+		assertEquals(13.058666, ntna6.calculateInterest(dtp, dtup, interest), 1e-6);
+		
+	}
 }
