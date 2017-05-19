a99028521da8b46fd77b4000fae19b11e30c46f2
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-19 00:16:58 -0300

    Change methods in LFT and extract class SelicTax
    Signed-off-by: Mateus Manuel <mateusmanuel21@hotmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- test/LFTTest.java
+++ test/LFTTest.java
@@ -6,21 +6,24 @@ import org.junit.Test;
 public class LFTTest {
 	
 	private LFT lft;
+	private SelicTax selicTax;
 	
 	@Before
 	public void setUp(){
-		lft = new LFT(1.000F, 3.0F);
+		lft = new LFT(3.0F);
+		selicTax = new SelicTax();
+		selicTax.calculateSelicTax(1.0F);
 	}
     
 	@Test
 	public void testSelicTaxCalculation() throws Exception {
-		assertEquals(lft.calculateSelicTax(), 0.000039486F, (float)1.0E-8F);
+		assertEquals(0.000039486F, selicTax.getSelicTax(), (float)1.0E-8F);
 	}
 	
 	
 	@Test
 	public void testActualNominalValueCalculation() throws Exception {
-		assertEquals(lft.calculateActualNominal(), 0.00011844F, (float)1.0E-6F);
+		assertEquals(0.00011844F, lft.calculateActualNominal(selicTax), (float)1.0E-6F);
 	}
 	
 	
