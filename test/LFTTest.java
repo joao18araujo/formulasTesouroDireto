c5fb6ada9ddc5a80b2225dadb15b311965a61aef
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-19 10:41:59 -0300

    Creating test to result factor for SelicTax class
    Signed-off-by: Mateus Manuel <mateusmanuel21@hotmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- test/LFTTest.java
+++ test/LFTTest.java
@@ -1,4 +1,9 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,8 +14,10 @@ public class LFTTest {
 	private SelicTax selicTax;
 	
 	@Before
-	public void setUp(){
+	public void setUp() throws Exception{
 		lft = new LFT(3.0F);
+		lft.setBaseDate(new SimpleDateFormat("dd/MM/yyyy").parse("15/05/2017"));
+		lft.setEndDate(new SimpleDateFormat("dd/MM/yyyy").parse("18/05/2017"));
 		selicTax = new SelicTax();
 		selicTax.calculateSelicTax(1.0F);
 	}
@@ -20,10 +27,24 @@ public class LFTTest {
 		assertEquals(0.000039486F, selicTax.getSelicTax(), (float)1.0E-8F);
 	}
 	
-	
 	@Test
 	public void testActualNominalValueCalculation() throws Exception {
 		assertEquals(0.00011844F, lft.calculateActualNominal(selicTax), (float)1.0E-6F);
+	}
+	
+	@Test
+	public void testPeriodConverionInDays() throws Exception {		
+		assertEquals(3, selicTax.calculatePeriodDays(lft.getBaseDate(), lft.getEndDate()));
+	}
+		
+	@Test 
+	public void testResultFactor() throws Exception {
+		List <SelicTax> selicTaxs = new ArrayList<SelicTax>();
+		selicTaxs.add(new SelicTax(1.0F));
+		selicTaxs.add(new SelicTax(1.1F));
+		selicTaxs.add(new SelicTax(1.2F));
+		
+		assertEquals(1.0001302F, selicTax.calculateResultFactor(lft.getBaseDate(), lft.getEndDate(), selicTaxs), (float)1.0E-8F);
 	}
 	
 	
