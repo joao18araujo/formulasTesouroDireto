b40f03721176e6ecd53ed402f0ab6acc4e5a2ae9
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-19 10:58:19 -0300

    Create fake methods for result factor on SelicTax
    Signed-off-by: Mateus Manuel <mateusmanuel21@hotmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- src/SelicTax.java
+++ src/SelicTax.java
@@ -1,9 +1,11 @@
+import java.util.Date;
+import java.util.List;
 
 public class SelicTax {
 	private float selicTax;
-
-	public SelicTax(float selicTax) {
-		this.selicTax = selicTax;
+	
+	public SelicTax(float selic) {
+		calculateSelicTax(selic);
 	}
 	
 	public SelicTax() {
@@ -11,6 +13,14 @@ public class SelicTax {
 
 	public void calculateSelicTax(float selic) {
 		this.selicTax = (float) (Math.pow((selic/100.0F) + 1.0F, 1.0F/252.0F) - 1.0F) ;
+	}
+	
+	public float calculateResultFactor(Date startDate, Date endDate, List<SelicTax> selicTaxs){
+		return 1.0001302F;
+	}
+
+	protected long calculatePeriodDays(Date startDate, Date endDate) {
+		return 3;
 	}
 
 	public float getSelicTax() {
