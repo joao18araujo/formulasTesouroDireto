4df98dfd7666dd3600495943aa8865c2b853fb1f
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-19 14:40:03 -0300

    Creating blank calculateUnitaryPrice method

---
+++ src/LTN.java
@@ -0,0 +1,16 @@
+
+public class LTN {
+	float tind;
+	
+	public float getTind(){
+		return tind;
+	}
+	
+	public void setTind(float Tind){
+		tind = Tind;
+	}
+	
+	public float calculateUnitaryPrice(){
+		return 0;
+	}
+}
