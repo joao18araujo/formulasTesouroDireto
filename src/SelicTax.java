48281abadb4df2ce6716825a942b5b19a547847d
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-19 11:19:18 -0300

    Implementing method on SelicTax to calculate result factor
    Signed-off-by: Mateus Manuel <mateusmanuel21@hotmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

--- src/SelicTax.java
+++ src/SelicTax.java
@@ -1,26 +1,37 @@
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SelicTax {
 	private float selicTax;
-	
+
 	public SelicTax(float selic) {
 		calculateSelicTax(selic);
 	}
-	
+
 	public SelicTax() {
 	}
 
 	public void calculateSelicTax(float selic) {
 		this.selicTax = (float) (Math.pow((selic/100.0F) + 1.0F, 1.0F/252.0F) - 1.0F) ;
 	}
-	
+
 	public float calculateResultFactor(Date startDate, Date endDate, List<SelicTax> selicTaxs){
-		return 1.0001302F;
+		long periodDays = calculatePeriodDays(startDate, endDate);
+
+		float productResult = (float) (1.0 + selicTaxs.get(0).getSelicTax());
+		for(long k = 1; k < periodDays; ++k) {
+			productResult *= (1.0 + selicTaxs.get((int) k).getSelicTax());
+		}
+
+		return productResult;
 	}
 
 	protected long calculatePeriodDays(Date startDate, Date endDate) {
-		return 3;
+		long diff = endDate.getTime() - startDate.getTime();
+		long periodDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+
+		return periodDays;
 	}
 
 	public float getSelicTax() {
