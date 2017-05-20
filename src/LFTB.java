685241bcbfbfd4736c2c4a404a6d9da3b12a81bf
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-20 17:13:40 -0300

    Creating LFTB class
    Signed-off-by: Mateus Manuel <mateusmanuel21@hotmail.com>
    Signed-off-by: João Vitor Araujo <joao18araujo@gmail.com>

---
+++ src/LFTB.java
@@ -0,0 +1,53 @@
+
+public class LFTB {
+	
+	private long du;
+	private long dut;
+	private int p;
+	private int n;
+	private float ve;
+	
+	public float calculateActualNominal(float c) {
+		return 135.0069F;
+	}	
+	
+	public float getVe() {
+		return ve;
+	}
+
+	public void setVe(float ve) {
+		this.ve = ve;
+	}
+
+	public long getDu() {
+		return du;
+	}
+	
+	public void setDu(long du) {
+		this.du = du;
+	}
+	
+	public long getDut() {
+		return dut;
+	}
+	
+	public void setDut(long dut) {
+		this.dut = dut;
+	}
+	
+	public int getP() {
+		return p;
+	}
+	
+	public void setP(int p) {
+		this.p = p;
+	}
+	
+	public int getN() {
+		return n;
+	}
+	
+	public void setN(int n) {
+		this.n = n;
+	}
+}
