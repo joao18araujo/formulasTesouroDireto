7b30da7c6656db546f40b3b4dbec840db095cbff
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-21 15:42:46 -0300

    Applying triangulation in tests and parameterizing test

--- test/NTNBTest.java
+++ test/NTNBTest.java
@@ -1,34 +1,39 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class NTNBTest {
 	
 	NTNB ntnb;
+	double expected;
 	
-	@Before
-	public void setUp(){
+	public NTNBTest(double IPCAt1, double IPCAt0, int dc, int dct, double expected){
 		ntnb = new NTNB();
+		ntnb.setIPCAt1(IPCAt1);
+		ntnb.setIPCAt0(IPCAt0);
+		ntnb.setDc(dc);
+		ntnb.setDct(dct);
+		
+		this.expected = expected;
+	}
+	
+	@Parameters
+	public static Iterable data(){
+		return Arrays.asList(new Object[][]{
+			{1.43, 1.01, 20, 34, 1.226964},
+			{2.04, 1.35, 35, 60, 1.272300},
+			{1.56, 1.00, 13, 20, 1.335152}
+		});
 	}
 	
 	@Test
 	public void testNominalValueFactorCalculation(){
-		ntnb.setIPCAt1(1.43);
-		ntnb.setIPCAt0(1.01);
-		ntnb.setDc(20);
-		ntnb.setDct(34);
-		
-		assertEquals(1.226964, ntnb.calculateNominalValueFactor(), 1e-6);
-	}
-
-	@Test
-	public void testNominalValueFactorCalculation2(){
-		ntnb.setIPCAt1(2.04);
-		ntnb.setIPCAt0(1.35);
-		ntnb.setDc(35);
-		ntnb.setDct(60);
-		
-		assertEquals(1.272300, ntnb.calculateNominalValueFactor(), 1e-6);
-	}
-
-	
+		assertEquals(expected, ntnb.calculateNominalValueFactor(), 1e-6);
+	}	
 }
