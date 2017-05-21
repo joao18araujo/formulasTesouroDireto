c3a0ce9f94f5d4054ebf7de962498b9ec2428fca
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-21 15:13:52 -0300

    Creating blank method

---
+++ src/NTNB.java
@@ -0,0 +1,45 @@
+
+public class NTNB {
+	private double IPCAt1;
+	private double IPCAt0;
+	private int dc;
+	private int dct;
+	
+	public double getIPCAt1() {
+		return IPCAt1;
+	}
+	
+	public void setIPCAt1(double iPCAt1) {
+		IPCAt1 = iPCAt1;
+	}
+	
+	public double getIPCAt0() {
+		return IPCAt0;
+	}
+	
+	public void setIPCAt0(double iPCAt0) {
+		IPCAt0 = iPCAt0;
+	}
+	
+	public int getDc() {
+		return dc;
+	}
+	
+	public void setDc(int dc) {
+		this.dc = dc;
+	}
+	
+	public int getDct() {
+		return dct;
+	}
+	
+	public void setDct(int dct) {
+		this.dct = dct;
+	}
+
+	public double calculateNominalValueFactor() {
+		return 0;
+	}
+	
+	
+}
