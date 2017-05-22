a44df0425a25e280e1e4ce26ab41ab0a4416d1a8
Mateus Manuel <mateusmanuel21@hotmail.com>
2017-05-22 20:49:51 -0300

    Creating class NTNBInterest with blank method

---
+++ src/NTNBInterest.java
@@ -0,0 +1,46 @@
+
+public class NTNBInterest {
+
+	double i;
+	int n;
+	int dcp;
+	int dct;
+	
+
+	public double calculateIntestFactor() {
+		return 0;
+	}
+	
+	public double getI() {
+		return i;
+	}
+	
+	public void setI(double i) {
+		this.i = i;
+	}
+	
+	public int getN() {
+		return n;
+	}
+	
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+	public int getDcp() {
+		return dcp;
+	}
+	
+	public void setDcp(int dcp) {
+		this.dcp = dcp;
+	}
+	
+	public int getDct() {
+		return dct;
+	}
+	
+	public void setDct(int dct) {
+		this.dct = dct;
+	}
+	
+}
