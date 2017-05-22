f815df853124f0f1f42b760728eb200ad086ad58
João Vitor Araujo <joao18araujo@gmail.com>
2017-05-21 21:56:29 -0300

    Creating first test to calculateNominalValue

--- test/NTNBTest.java
+++ test/NTNBTest.java
@@ -11,29 +11,39 @@ import org.junit.runners.Parameterized.Parameters;
 public class NTNBTest {
 	
 	NTNB ntnb;
-	double expected;
+	double nominalValueExpected, nominalValueFactorExpected;
 	
-	public NTNBTest(double IPCAt1, double IPCAt0, int dc, int dct, double expected){
+	public NTNBTest(double IPCAt1, double IPCAt0, int dc, int dct, double nominalValueFactorExpected, 
+					double IPCAt, double VN, double nominalValueExpected){
 		ntnb = new NTNB();
 		ntnb.setIPCAt1(IPCAt1);
 		ntnb.setIPCAt0(IPCAt0);
 		ntnb.setDc(dc);
 		ntnb.setDct(dct);
+		ntnb.setIPCAt(IPCAt);
+		ntnb.setVN(VN);
 		
-		this.expected = expected;
+		this.nominalValueFactorExpected = nominalValueFactorExpected;
+		this.nominalValueExpected = nominalValueExpected;
 	}
 	
 	@Parameters
 	public static Iterable data(){
 		return Arrays.asList(new Object[][]{
-			{1.43, 1.01, 20, 34, 1.226964},
-			{2.04, 1.35, 35, 60, 1.272300},
-			{1.56, 1.00, 13, 20, 1.335152}
+			{1.43, 1.01, 20, 34, 1.226964, 1.63, 2508.90, 4967.997887},
+			{2.04, 1.35, 35, 60, 1.272300, 0, 0, 0},
+			{1.56, 1.00, 13, 20, 1.335152, 0, 0, 0}
 		});
 	}
 	
 	@Test
 	public void testNominalValueFactorCalculation(){
-		assertEquals(expected, ntnb.calculateNominalValueFactor(), 1e-6);
-	}	
+		assertEquals(nominalValueFactorExpected, ntnb.calculateNominalValueFactor(), 1e-6);
+	}
+	
+	@Test
+	public void testNominalValueCalculation(){
+		assertEquals(nominalValueExpected, ntnb.calculateNominalValue(), 1e-6);
+	}
+
 }
